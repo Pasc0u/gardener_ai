084cea0f36119a7f70783b4216951dd2fa8ed251
Lena Geisler <lenageisler@gmx.de>
2025-08-26 15:42:44 +0200

    added chats model

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,5 +1,6 @@
 class Plant < ApplicationRecord
   belongs_to :user
+  has_many :chats
 
   validates :species, :is_potted, :location, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_26_133042) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_26_134046) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chats", force: :cascade do |t|
+    t.string "topic"
+    t.bigint "plant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["plant_id"], name: "index_chats_on_plant_id"
+  end
 
   create_table "plants", force: :cascade do |t|
     t.string "nickname"
@@ -41,5 +49,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_26_133042) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "chats", "plants"
   add_foreign_key "plants", "users"
 end
