5f04a00201d02fb4744340fd9f1329a352c716de
Pascaline Dierckx <pascaline.dierckx@gmail.com>
2025-08-27 15:43:21 +0200

    set up ruby_llm

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,32 +1,30 @@
 class MessagesController < ApplicationController
 SYSTEM_PROMPT = "You are a gardener with 30 years of experience.
 
-I have some plants at home or in my garden, quite new to taking care of plants.
+I have some plants at home or in my garden, I'm quite new to taking care of plants.
 
 Help me take care of my plants so that they can grow well.
 
 Answer concisely in markdown."
 
-  def index
-    @messages = Message.all
-  end
-
   def create
     @chat = Chat.find(params[:chat_id])
-    @message = Message.new(role: "user", content: params[:message][:content], chat: @chat)
+    @message = Message.new(message_params)
+    @message.role = "user"
+    @message.chat = @chat
     if @message.save
-      @chat = RubyLLM.save
-      response = @chat.with_instructions(SYSTEM_PROMPT).ask(@message.content)
+      @LLMchat = RubyLLM.chat
+      response = @LLMchat.with_instructions(SYSTEM_PROMPT).ask(@message.content)
       Message.create(role: "assistant", content: response.content, chat: @chat)
       redirect_to chat_path(@chat)
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # private
+  private
 
-  # def message_params
-  #   params.require(:message).permit(:content)
-  # end
+  def message_params
+    params.require(:message).permit(:content, :file)
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :chat
+  has_one :plant, through: :chat
 
   validates :content, :role, presence: true
 end
