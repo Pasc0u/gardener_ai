52a606ef00e3bf943e4dbe9f3a1d307c02d691cf
Pascaline Dierckx <pascaline.dierckx@gmail.com>
2025-08-27 16:15:22 +0200

    added validations for our message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,17 @@ class Message < ApplicationRecord
   has_one_attached :file
 
   validates :content, :role, presence: true
+
+  validate :file_size_validation
+  validates :content, length: { minimum: 10, maximum: 1000 }, if: -> { role == "user" }
+
+  MAX_FILE_SIZE_MB = 10
+
+  private
+
+  def file_size_validation
+    if file.attached? && file.byte_size > MAX_FILE_SIZE_MB.megabytes
+      errors.add(:file, "size must be less than #{MAX_FILE_SIZE_MB}MB")
+    end
+  end
 end
