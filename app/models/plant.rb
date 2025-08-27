7de958f7725a4cf2f9a53f99c618687bc682acf5
Franklin Rosas <113691934+Frank-RedRoses@users.noreply.github.com>
2025-08-27 11:40:53 +0200

    Fix: isPotted variable false validation

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -2,5 +2,5 @@ class Plant < ApplicationRecord
   belongs_to :user
   has_many :chats
 
-  validates :species, :is_potted, :location, presence: true
+  validates :species, :location, presence: true
 end
