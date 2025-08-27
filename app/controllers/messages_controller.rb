38b4ef6f66563c872ff415fdf2032bdb76800f32
Pascaline Dierckx <pascaline.dierckx@gmail.com>
2025-08-27 16:10:03 +0200

    finished message create action with active record and rubyLLM

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,9 +13,12 @@ Answer concisely in markdown."
     @message.role = "user"
     @message.chat = @chat
     if @message.save
-      @LLMchat = RubyLLM.chat
-      response = @LLMchat.with_instructions(SYSTEM_PROMPT).ask(@message.content)
-      Message.create(role: "assistant", content: response.content, chat: @chat)
+      if @message.file.attached?
+        process_file(@message.file)
+      else
+        send_question
+      end
+      Message.create(role: "assistant", content: @response.content, chat: @chat)
       redirect_to chat_path(@chat)
     else
       render :new, status: :unprocessable_entity
@@ -23,6 +26,19 @@ Answer concisely in markdown."
   end
 
   private
+
+  def send_question(model: "gpt-4.1-nano", with: {})
+    @LLMchat = RubyLLM.chat(model: model)
+    @response = @LLMchat.with_instructions(SYSTEM_PROMPT).ask(@message.content, with: with)
+  end
+
+  def process_file(file)
+    if file.content_type == "application/pdf"
+      send_question(model: "gemini-2.0-flash", with: { pdf: @message.file.url })
+    elsif file.image?
+      send_question(model: "gpt-4o", with: { image: @message.file.url })
+    end
+  end
 
   def message_params
     params.require(:message).permit(:content, :file)
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :chat
   has_one :plant, through: :chat
+  has_one_attached :file
 
   validates :content, :role, presence: true
 end
