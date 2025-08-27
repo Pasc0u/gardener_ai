4cab2e32fccfd5e4942015567c305a8c44527d85
Pascaline Dierckx <pascaline.dierckx@gmail.com>
2025-08-27 11:31:29 +0200

    fixed double routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-  get 'messages/new'
-  get 'messages/create'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
